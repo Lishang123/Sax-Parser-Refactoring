52cb170be8669b91905600a07422ba3578f60c45
houcai <lishannng@hotmail.com>
2026-02-22 19:38:25 +0100

    refactor: wrap M_SystemMessage into an error class derived from std::runtime_error

---
+++ Misc/SystemMessageError.hpp
@@ -0,0 +1,26 @@
+#ifndef CPP_TASK_SYSTEMERRORMESSAGE_HPP
+#define CPP_TASK_SYSTEMERRORMESSAGE_HPP
+
+class SystemMessageError final : public std::runtime_error {
+public:
+    explicit SystemMessageError(M_SystemMessage msg)
+        : std::runtime_error(toString(msg))
+        , msg_(std::move(msg)) {}
+
+    [[nodiscard]] const M_SystemMessage& message() const noexcept { return msg_; }
+
+private:
+    M_SystemMessage msg_;
+
+    static std::string toString(const M_SystemMessage& m) {
+        std::string s;
+        s += m.getDomain();
+        s += ":";
+        s += m.getCode();
+        s += ": ";
+        s += m.getDescription();
+        return s;
+    }
+};
+
+#endif //CPP_TASK_SYSTEMERRORMESSAGE_HPP
