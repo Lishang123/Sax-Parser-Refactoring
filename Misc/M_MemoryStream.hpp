7971706ab29ffbda82734158a6361df60e26108d
houcai <lishannng@hotmail.com>
2026-03-09 17:02:19 +0100

    refactor(M_MemoryStream): add override to overriding destructor and remove virtual

--- Misc/M_MemoryStream.hpp
+++ Misc/M_MemoryStream.hpp
@@ -119,7 +119,8 @@ class  M_MemoryStream : public M::IReadStream
 		M_MemoryStream( M_MemoryStream&& src) noexcept;
 		M_MemoryStream( const char* String);
 		M_MemoryStream( const TY_Blob* Buffer);
-		virtual ~M_MemoryStream() = default;
+
+		~M_MemoryStream() override = default;
 
 		M_MemoryStream& operator =( const M_MemoryStream& src) = delete;
 		M_MemoryStream& operator =( M_MemoryStream&& src) noexcept;
